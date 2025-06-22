e1c40a6a2f1c59a1c5bb4a08e5decd3b4520a8ad
Queijitos <walter.jose@alunoescolassociais.org.br>
2025-06-22 00:01:30 -0300

    arrumei

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,3 +1,16 @@
 const quokka = { isAwesome: true, nodeVersion: process.version };
 
 console.log(quokka);// Função que verifica se o número é par
+
+
+const container = document.getElementById('cardContainer');
+const nextBtn = document.getElementById('nextBtn');
+const prevBtn = document.getElementById('prevBtn');
+
+nextBtn.addEventListener('click', () => {
+    container.scrollBy({ left: 320, behavior: 'smooth' });
+});
+
+prevBtn.addEventListener('click', () => {
+    container.scrollBy({ left: -320, behavior: 'smooth' });
+});
